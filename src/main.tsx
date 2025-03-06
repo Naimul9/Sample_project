ad07ebbd398fc6e93378328942c5f31637e240eb
Naimul9 <naimulakib100@gmail.com>
2025-03-06 17:29:51 +0000

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,11 @@ import { LanguageProvider } from "./context/LanguageContext";
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
 
-const basename = import.meta.env.BASE_URL;
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <LanguageProvider>
       <ThemeProvider>
-        <BrowserRouter basename={basename}>
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </ThemeProvider>
